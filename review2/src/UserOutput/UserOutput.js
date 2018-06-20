0b8737b54a956f854e85c4d38e113ebef5deac46
Jonathan Aqauarone <J.aquarone@gmail.com>
2018-06-20 13:24:32 -0400

    completed tasks 1 , 2 and 3

---
+++ review2/src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const userOutput = () => {
+  return (
+      <div>
+        <p>Some random text </p>
+        <p> Some more radnom text</p>
+      </div>  
+  );
+};
+
+
+export default userOutput;
